2ae4f1255ff7b1ff790e1d0e052f878863d1e19e
Jim Wang <turanuk@gmail.com>
2012-12-28 15:26:39 -0800

    Prototyping for ViewTeamList page

--- Scripts/TeamListViewModel.js
+++ Scripts/TeamListViewModel.js
@@ -1,4 +1,30 @@
 var TeamListViewModel = function () {
   var self = this;
+  self.teams = ko.observableArray([{ "Id": 25, "Name": "Foo"}]);
 
+  //This passes a data- payload to the modal dialog's "Save" button that contains the teamId.
+  self.sharePrep = function (team) {
+    $('#shareButton').attr('data-teamId', team.Id);
+  }
+
+  self.share = function (viewModel, event) {
+    var teamId = $(event.srcElement).attr('data-teamId');
+
+  }
+
+  self.newTeam = function () {
+    alert('newTeam');
+  }
+
+  self.settings = function (team) {
+    alert('settings');
+  }
+
+  $.getJSON('/endpoints/getteamlist', function (teams) {
+    self.teams(teams);
+  })
 }
+
+$(function () {
+  ko.applyBindings(new TeamListViewModel());
+});
