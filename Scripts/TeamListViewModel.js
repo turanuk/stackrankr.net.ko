72e768accec4c43bb536bbc63dfa2ad2294a2872
Jim Wang <turanuk@gmail.com>
2012-12-28 18:01:50 -0800

    Additional dialog prototyping, adding the "new team" feature

--- Scripts/TeamListViewModel.js
+++ Scripts/TeamListViewModel.js
@@ -1,15 +1,21 @@
 var TeamListViewModel = function () {
   var self = this;
-  self.teams = ko.observableArray([{ "Id": 25, "Name": "Foo"}]);
+  self.teams = ko.observableArray();
 
-  //This passes a data- payload to the modal dialog's "Save" button that contains the teamId.
+  //This passes a data- payload to the modal dialog's "Save" button that contains the teamId, and clears out the input.
   self.sharePrep = function (team) {
     $('#shareButton').attr('data-teamId', team.Id);
+    ('#usernameInput').val('')
   }
 
   self.share = function (viewModel, event) {
     var teamId = $(event.srcElement).attr('data-teamId');
 
+  }
+
+  //This clears out the input to the dialog 
+  self.newTeamPrep = function () {
+    $('#newTeamInput').val('');
   }
 
   self.newTeam = function () {
